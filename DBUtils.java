a1245b9f94f7d8b6caa1cd0280decf831a12ef66
TheAmitDeokar <49775536+TheAmitDeokar@users.noreply.github.com>
2024-12-22 21:29:00 +0530

    Update DBUtils.java

--- DBUtils.java
+++ DBUtils.java
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-I am editing from main branch
-=======
-I am editin from dev branch
->>>>>>> development
-I am in Dev branch
-
-Hello I am updating tthis again file
-
 Hello Guys
 This is java file.
 
